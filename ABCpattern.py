999c4a21ee4bbbbe6337036d766284c1d99d55d1
Vidhi Shakya <vidhishakya06@gmail.com>
2025-08-14 10:55:35 +0530

    ABC

--- ABCpattern.py
+++ ABCpattern.py
@@ -1,9 +1,9 @@
 # ABCpattern.py
 
-n=int(input("Enter ASCII value of starting character:  "))
-m=int(input("Enter ASCII value of ending character:  "))
 
-for i in range (n,m+1):
-    for j in range(n, i+1):
-        print(chr(j), end="")
+
+n=int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(chr(64+j), end='')
     print()
