4e74a4c9560a45a93fa46f0b1b8e6f753af7eeef
Vidhi Shakya <vidhishakya06@gmail.com>
2025-08-14 14:45:21 +0530

    cw

---
+++ factorial.py
@@ -0,0 +1,10 @@
+#  factorial of a number
+
+num= int(input("Enter the number: "))
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n * factorial (n-1)
+print(factorial(num))
