2e1df190bae8a64d5394730bd1e4fc12439c75ba
Vidhi Shakya <vidhishakya06@gmail.com>
2025-08-15 11:06:03 +0530

    pattern3ss_file

---
+++ Pattern3ss.py/pattern3.py
@@ -0,0 +1,16 @@
+
+n= int(input("Enter number of rows: " ))
+
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(chr(64+j), end=' ')
+    k=n-(i-1)
+    for j in range(1,k):
+        print(" ",end=' ')
+    for j in range(2,k):
+        print(" ",end=' ')      
+    for j in range(i,0,-1):
+        if (j==n):
+            continue
+        print(chr(64+j), end=' ')
+    print()
---
+++ Pattern3ss.py/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ # for j in range(2,k):
+    #     print(" ",end=' ') 
